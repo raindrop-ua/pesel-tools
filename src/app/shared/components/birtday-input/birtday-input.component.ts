a1e12ba9aab4941c2b3a8b7193e14938442b1024
Anton Sizov <raindropua@gmail.com>
2025-05-16 10:32:08 +0300

    Add placeholders

---
+++ src/app/shared/components/birtday-input/birtday-input.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-birtday-input',
+  imports: [],
+  templateUrl: './birtday-input.component.html',
+  styleUrl: './birtday-input.component.scss'
+})
+export class BirtdayInputComponent {
+
+}
---
+++ src/app/shared/components/pesel-output/pesel-output.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pesel-output',
+  imports: [],
+  templateUrl: './pesel-output.component.html',
+  styleUrl: './pesel-output.component.scss'
+})
+export class PeselOutputComponent {
+
+}
