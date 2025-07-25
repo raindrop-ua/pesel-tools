fca34d0ec74c05398aeaf3212b340f2e51484afd
Anton Sizov <raindropua@gmail.com>
2025-07-25 19:47:02 +0300

    Add validator feature

---
+++ src/app/features/validator/validator.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-validator',
+  imports: [],
+  templateUrl: './validator.component.html',
+  styleUrl: './validator.component.scss'
+})
+export class ValidatorComponent {
+
+}
