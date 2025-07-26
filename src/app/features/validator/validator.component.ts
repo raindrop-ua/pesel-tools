c252b833dfe4ffdab34e43b9126adf8d8d0cb3a3
Anton Sizov <raindropua@gmail.com>
2025-07-26 08:46:46 +0300

    Add PESEL input and PESEL output components

--- src/app/features/validator/validator.component.ts
+++ src/app/features/validator/validator.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { SectionComponent } from '../../core/layout/section/section.component';
+import { CardComponent } from '../../shared/components/card/card.component';
+import { PeselInputComponent } from '../../shared/components/pesel-input/pesel-input.component';
+import { PeselOutputComponent } from '../../shared/components/pesel-output/pesel-output.component';
 
 @Component({
   selector: 'app-validator',
-  imports: [],
+  imports: [
+    SectionComponent,
+    CardComponent,
+    PeselInputComponent,
+    PeselOutputComponent,
+  ],
   templateUrl: './validator.component.html',
-  styleUrl: './validator.component.scss'
+  styleUrl: './validator.component.scss',
 })
-export class ValidatorComponent {
-
-}
+export class ValidatorComponent {}
---
+++ src/app/shared/components/pesel-input/pesel-input.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pesel-input',
+  imports: [],
+  templateUrl: './pesel-input.component.html',
+  styleUrl: './pesel-input.component.scss'
+})
+export class PeselInputComponent {
+
+}
