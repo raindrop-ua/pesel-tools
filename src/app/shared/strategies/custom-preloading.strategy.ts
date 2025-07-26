d763e7c04759523c5c5148260a1d34473d036789
Anton Sizov <raindropua@gmail.com>
2025-07-26 09:19:34 +0300

    Add PESEL input and PESEL output components

--- src/app/shared/strategies/custom-preloading.strategy.ts
+++ src/app/shared/strategies/custom-preloading.strategy.ts
@@ -4,7 +4,7 @@ import { Observable, of } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class CustomPreloadingStrategy implements PreloadingStrategy {
-  preload(route: Route, load: () => Observable<any>): Observable<any> {
+  preload(route: Route, load: () => Observable<unknown>): Observable<unknown> {
     if (route.data && route.data['preload']) {
       return load();
     } else {
