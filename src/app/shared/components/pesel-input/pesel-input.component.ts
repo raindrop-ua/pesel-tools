e1b9197cd65e480565159cd522144b3790255601
Anton Sizov <raindropua@gmail.com>
2025-07-26 20:46:58 +0300

    Add Husky and stylelint

--- src/app/shared/components/pesel-input/pesel-input.component.ts
+++ src/app/shared/components/pesel-input/pesel-input.component.ts
@@ -1,4 +1,4 @@
-import { Component, forwardRef, input, Input } from '@angular/core';
+import { Component, forwardRef, input } from '@angular/core';
 import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 
 @Component({
