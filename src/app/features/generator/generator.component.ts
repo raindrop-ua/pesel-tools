d08147b2597ce1e473fa140d48356ccd8edbcad4
Anton Sizov <raindropua@gmail.com>
2025-07-26 16:03:40 +0300

    Update to signals

--- src/app/features/generator/generator.component.ts
+++ src/app/features/generator/generator.component.ts
@@ -1,26 +1,25 @@
-import { Component } from '@angular/core';
-import {CardComponent} from "../../shared/components/card/card.component";
-import {FormsModule, ReactiveFormsModule} from "@angular/forms";
-import {PeselOutputComponent} from "../../shared/components/pesel-output/pesel-output.component";
-import {SectionComponent} from "../../core/layout/section/section.component";
+import { Component, signal } from '@angular/core';
+import { CardComponent } from '../../shared/components/card/card.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { PeselOutputComponent } from '../../shared/components/pesel-output/pesel-output.component';
+import { SectionComponent } from '../../core/layout/section/section.component';
+import { BirthdayInputComponent } from '../../shared/components/birthday-input/birthday-input.component';
+import { ButtonComponent } from '../../shared/components/button/button.component';
 
 @Component({
   selector: 'app-generator',
-    imports: [
-        CardComponent,
-        FormsModule,
-        PeselOutputComponent,
-        ReactiveFormsModule,
-        SectionComponent
-    ],
+  imports: [
+    CardComponent,
+    FormsModule,
+    PeselOutputComponent,
+    ReactiveFormsModule,
+    SectionComponent,
+    BirthdayInputComponent,
+    ButtonComponent,
+  ],
   templateUrl: './generator.component.html',
-  styleUrl: './generator.component.scss'
+  styleUrl: './generator.component.scss',
 })
 export class GeneratorComponent {
-  pesels: string[] = [
-    '91062800008',
-    '82090519293',
-    '13302111183',
-  ];
-
+  pesels = signal<string[]>(['91062800008', '82090519293', '13302111183']);
 }
--- src/app/shared/components/birthday-input/birthday-input.component.ts
+++ src/app/shared/components/birthday-input/birthday-input.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { PeselInputComponent } from '../pesel-input/pesel-input.component';
 
 @Component({
   selector: 'app-birthday-input',
-  imports: [],
+  imports: [PeselInputComponent],
   templateUrl: './birthday-input.component.html',
-  styleUrl: './birthday-input.component.scss'
+  styleUrl: './birthday-input.component.scss',
 })
-export class BirthdayInputComponent {
-
-}
+export class BirthdayInputComponent {}
