df786bbe534e36dd1efb151bf564890c25c244b5
Anton Sizov <raindropua@gmail.com>
2025-07-26 17:29:27 +0300

    Add hero image

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,13 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
-import { ButtonComponent } from '../../shared/components/button/button.component';
-import {SectionComponent} from '../../core/layout/section/section.component';
-import {CardComponent} from '../../shared/components/card/card.component';
-import {DisclaimerComponent} from '../../shared/components/disclaimer/disclaimer.component';
+import { SectionComponent } from '../../core/layout/section/section.component';
+import { CardComponent } from '../../shared/components/card/card.component';
+import { DisclaimerComponent } from '../../shared/components/disclaimer/disclaimer.component';
 
 @Component({
   selector: 'app-home',
-  imports: [RouterLink, ButtonComponent, SectionComponent, CardComponent, DisclaimerComponent],
+  imports: [SectionComponent, CardComponent, DisclaimerComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
