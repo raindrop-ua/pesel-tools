a57ad15ae05b09ef569e822d6fd7f4261f214787
Anton Sizov <raindropua@gmail.com>
2025-07-26 12:00:12 +0300

    Add disclaimer component

--- src/app/features/generator/generator.component.ts
+++ src/app/features/generator/generator.component.ts
@@ -1,8 +1,6 @@
 import { Component } from '@angular/core';
-import {ButtonComponent} from "../../shared/components/button/button.component";
 import {CardComponent} from "../../shared/components/card/card.component";
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
-import {PeselInputComponent} from "../../shared/components/pesel-input/pesel-input.component";
 import {PeselOutputComponent} from "../../shared/components/pesel-output/pesel-output.component";
 import {SectionComponent} from "../../core/layout/section/section.component";
 
