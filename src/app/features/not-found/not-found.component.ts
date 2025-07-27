695377e8ab3886dd9466be246bf447a4a24b3ee7
Anton Sizov <raindropua@gmail.com>
2025-07-27 11:07:37 +0300

    Fix masthead image size

--- src/app/features/not-found/not-found.component.ts
+++ src/app/features/not-found/not-found.component.ts
@@ -1,14 +1,11 @@
 import { Component } from '@angular/core';
 import {SectionComponent} from '../../core/layout/section/section.component';
+import { CardComponent } from '../../shared/components/card/card.component';
 
 @Component({
   selector: 'app-not-found',
-  imports: [
-    SectionComponent
-  ],
+  imports: [SectionComponent, CardComponent],
   templateUrl: './not-found.component.html',
-  styleUrl: './not-found.component.scss'
+  styleUrl: './not-found.component.scss',
 })
-export class NotFoundComponent {
-
-}
+export class NotFoundComponent {}
