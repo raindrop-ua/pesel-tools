fce8096bd41c1e6e060ec3f44a6ffe3afba632a5
Anton Sizov <raindropua@gmail.com>
2025-07-28 08:34:13 +0300

    Add mobile menu in header

--- src/app/core/layout/header/header.component.ts
+++ src/app/core/layout/header/header.component.ts
@@ -1,4 +1,10 @@
-import { Component, inject } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  HostListener,
+  inject,
+  signal,
+} from '@angular/core';
 import { Router, RouterLink } from '@angular/router';
 import { NAVIGATION_TOKEN } from '../../../config/navigation.config';
 import { ThemeSwitcherComponent } from '../../../shared/components/theme-switcher/theme-switcher.component';
@@ -8,10 +14,12 @@ import { ThemeSwitcherComponent } from '../../../shared/components/theme-switche
   imports: [RouterLink, ThemeSwitcherComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HeaderComponent {
   private router = inject(Router);
   navigation = inject(NAVIGATION_TOKEN);
+  menuOpen = signal(false);
 
   isLinkActive(path: string): boolean {
     return this.router.isActive(path, {
@@ -20,5 +28,21 @@ export class HeaderComponent {
       matrixParams: 'ignored',
       fragment: 'ignored',
     });
+  }
+
+  toggleMenu() {
+    this.menuOpen.update((value) => !value);
+  }
+
+  closeMenu() {
+    this.menuOpen.set(false);
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: UIEvent): void {
+    const width = (event.target as Window).innerWidth;
+    if (width > 767 && this.menuOpen) {
+      this.menuOpen.set(false);
+    }
   }
 }
