6c8a31494f075846a263cc69b4b28b46b1a59b75
Anton Sizov <raindropua@gmail.com>
2025-07-28 08:44:17 +0300

    Add navigation in footer

--- src/app/core/layout/footer/footer.component.ts
+++ src/app/core/layout/footer/footer.component.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
+import { NAVIGATION_TOKEN } from '../../../config/navigation.config';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.scss'
+  styleUrl: './footer.component.scss',
 })
 export class FooterComponent {
+  private router = inject(Router);
+  navigation = inject(NAVIGATION_TOKEN);
 
+  isLinkActive(path: string): boolean {
+    return this.router.isActive(path, {
+      paths: 'exact',
+      queryParams: 'ignored',
+      matrixParams: 'ignored',
+      fragment: 'ignored',
+    });
+  }
 }
