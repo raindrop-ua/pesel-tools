7c57ace7906066d8b49c58e74b8e7b549a8e07b7
Anton Sizov <raindropua@gmail.com>
2025-07-29 14:49:25 +0300

    feat(value-input): add generic value input component

---
+++ src/app/shared/components/value-input/value-input.component.ts
@@ -0,0 +1,53 @@
+import {Component, forwardRef, input} from '@angular/core';
+import {NG_VALUE_ACCESSOR} from '@angular/forms';
+
+@Component({
+  selector: 'app-value-input',
+  imports: [],
+  templateUrl: './value-input.component.html',
+  styleUrl: './value-input.component.scss',
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => ValueInputComponent),
+      multi: true,
+    },
+  ],
+})
+export class ValueInputComponent {
+  placeholder = input<string>('');
+
+  value = '';
+  disabled = false;
+
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  private onChange: (value: string) => void = () => {};
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  private onTouched: () => void = () => {};
+
+  writeValue(value: string): void {
+    this.value = value ?? '';
+  }
+
+  registerOnChange(fn: (value: string) => void): void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: () => void): void {
+    this.onTouched = fn;
+  }
+
+  setDisabledState(isDisabled: boolean): void {
+    this.disabled = isDisabled;
+  }
+
+  handleInput(event: Event): void {
+    const newValue = (event.target as HTMLInputElement).value;
+    this.value = newValue;
+    this.onChange(newValue);
+  }
+
+  handleBlur(): void {
+    this.onTouched();
+  }
+}
