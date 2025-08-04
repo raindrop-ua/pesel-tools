de073afac547b2ef3782269d913ba0ca41080aa8
Anton Sizov <raindropua@gmail.com>
2025-08-04 09:52:15 +0300

    chore(inputs): change input element type

--- src/app/shared/components/disclaimer/disclaimer.component.ts
+++ src/app/shared/components/disclaimer/disclaimer.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import {ExternalLinkDirective} from '@shared/directives/external-link.directive';
 
 @Component({
   selector: 'app-disclaimer',
-  imports: [],
+  imports: [
+    ExternalLinkDirective
+  ],
   templateUrl: './disclaimer.component.html',
   styleUrl: './disclaimer.component.scss'
 })
