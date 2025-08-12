0710d7640e15c13eb7c676cf441b06c0e42c2569
Anton Sizov <raindropua@gmail.com>
2025-08-12 20:53:18 +0300

    feat(sw-update): add service for handling version updates and user prompt integration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { SeoService } from '@services/seo.service';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +7,4 @@ import { SeoService } from '@services/seo.service';
   templateUrl: './app.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AppComponent {
-  private _seo: SeoService = inject(SeoService);
-}
+export class AppComponent {}
