50684631d57ccadba91fd6cdac622313799fd0cf
Anton Sizov <raindropua@gmail.com>
2026-03-15 12:01:08 +0200

    chore: add API instruction component for PESEL Generator feature

---
+++ src/app/features/generator/components/api-instruction/api-instruction.component.ts
@@ -0,0 +1,10 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-api-instruction',
+  imports: [],
+  templateUrl: './api-instruction.component.html',
+  styleUrl: './api-instruction.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ApiInstructionComponent {}
