df74095a06ef33f4a66678c56caea53132575ddc
Anton Sizov <raindropua@gmail.com>
2026-03-15 12:01:15 +0200

    chore: replace inline PESEL Generator API section with ApiInstruction component

--- src/app/features/generator/feature/generator.component.ts
+++ src/app/features/generator/feature/generator.component.ts
@@ -3,6 +3,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { SectionComponent } from '@core/layout/section/section.component';
 import { SimpleGeneratorComponent } from '@features/generator/components/simple-generator/simple-generator.component';
 import {CardComponent} from '@components/card/card.component';
+import {ApiInstructionComponent} from '@features/generator/components/api-instruction/api-instruction.component';
 
 @Component({
   selector: 'app-generator',
@@ -12,6 +13,7 @@ import {CardComponent} from '@components/card/card.component';
     SectionComponent,
     SimpleGeneratorComponent,
     CardComponent,
+    ApiInstructionComponent,
   ],
   templateUrl: './generator.component.html',
   styleUrl: './generator.component.scss',
