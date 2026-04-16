50c2e4b1e303d221799616b533ab61db97eddcb2
Anton Sizov <raindropua@gmail.com>
2026-04-16 20:09:15 +0300

    feat: add path aliases in `vitest.config.ts` and initialize Angular test environment in `test-setup.ts`

--- src/test-setup.ts
+++ src/test-setup.ts
@@ -1,1 +1,11 @@
 import '@testing-library/jest-dom';
+import { TestBed } from '@angular/core/testing';
+import {
+  BrowserDynamicTestingModule,
+  platformBrowserDynamicTesting,
+} from '@angular/platform-browser-dynamic/testing';
+
+TestBed.initTestEnvironment(
+  BrowserDynamicTestingModule,
+  platformBrowserDynamicTesting(),
+);
--- vitest.config.ts
+++ vitest.config.ts
@@ -1,6 +1,21 @@
 import { defineConfig } from 'vitest/config';
+import { fileURLToPath, URL } from 'node:url';
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@config': fileURLToPath(new URL('./src/app/config', import.meta.url)),
+      '@core': fileURLToPath(new URL('./src/app/core', import.meta.url)),
+      '@shared': fileURLToPath(new URL('./src/app/shared', import.meta.url)),
+      '@features': fileURLToPath(
+        new URL('./src/app/features', import.meta.url),
+      ),
+      '@components': fileURLToPath(
+        new URL('./src/app/shared/components', import.meta.url),
+      ),
+      '@services': fileURLToPath(new URL('./src/app/core/services', import.meta.url)),
+    },
+  },
   test: {
     globals: true,
     environment: 'jsdom',
