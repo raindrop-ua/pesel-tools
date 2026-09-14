3feda914c55c94c1ff36ff3663fae0ce014c5e79
Anton Sizov <raindropua@gmail.com>
2026-09-14 17:10:04 +0300

    chore: refactor masthead component, remove unused animations, and update styles for improved maintainability

--- src/app/features/home/components/masthead/masthead.component.ts
+++ src/app/features/home/components/masthead/masthead.component.ts
@@ -1,59 +1,15 @@
-import {
-  AfterViewInit,
-  Component,
-  ElementRef,
-  ViewChild,
-  OnDestroy,
-  PLATFORM_ID,
-  inject,
-  ChangeDetectionStrategy,
-} from '@angular/core';
-import { isPlatformBrowser, NgOptimizedImage } from '@angular/common';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { NgOptimizedImage } from '@angular/common';
+import { RouterLink } from '@angular/router';
+import { AppRouteEnum } from '@core/enums/app-route.enum';
 
 @Component({
   selector: 'app-masthead',
-  imports: [NgOptimizedImage],
+  imports: [NgOptimizedImage, RouterLink],
   templateUrl: './masthead.component.html',
-  host: { class: 'relative mt-4 mb-8 block size-full' },
+  host: { class: 'mt-4 mb-8 block md:mt-10' },
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class MastheadComponent implements AfterViewInit, OnDestroy {
-  private platformId = inject(PLATFORM_ID);
-  private mouseMoveHandler?: (event: MouseEvent) => void;
-
-  @ViewChild('masthead') mastheadRef!: ElementRef;
-  @ViewChild('parallaxImg') imgRef!: ElementRef;
-
-  public ngAfterViewInit(): void {
-    if (!isPlatformBrowser(this.platformId) || this.isTouchDevice()) return;
-
-    const container = this.mastheadRef.nativeElement;
-    const image = this.imgRef.nativeElement;
-
-    this.mouseMoveHandler = (event: MouseEvent) => {
-      const rect = container.getBoundingClientRect();
-      const offsetX = (event.clientX - rect.left) / rect.width - 0.5;
-      const maxTranslate = 15;
-      const translateX = -offsetX * maxTranslate;
-
-      image.style.transform = `translate(${translateX}px)`;
-    };
-
-    container.addEventListener('mousemove', this.mouseMoveHandler, {
-      passive: true,
-    });
-  }
-
-  public ngOnDestroy(): void {
-    if (this.mouseMoveHandler && this.mastheadRef?.nativeElement) {
-      this.mastheadRef.nativeElement.removeEventListener(
-        'mousemove',
-        this.mouseMoveHandler,
-      );
-    }
-  }
-
-  private isTouchDevice(): boolean {
-    return navigator.maxTouchPoints > 0 || 'ontouchstart' in window;
-  }
+export class MastheadComponent {
+  protected readonly AppRouteEnum = AppRouteEnum;
 }
